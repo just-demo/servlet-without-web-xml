6eb5473abfb86d2855ee14548ed318ac2cf89d00
Anatolii <anatolii.kryvda@gmail.com>
2017-09-28 01:03:48 +0300

    Creating a simple servlet without web.xml

---
+++ src/main/java/self/ed/servlet/TestServlet.java
@@ -0,0 +1,19 @@
+package self.ed.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author Anatolii
+ */
+@WebServlet("/test")
+public class TestServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().print("Test!");
+    }
+}
